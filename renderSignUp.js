b60adb10ccc9faa012c03d79bf455b7235d9b6ee
tsionc <36338302+tsionc@users.noreply.github.com>
2019-12-10 00:23:08 -0500

    signup

---
+++ renderSignUp.js
@@ -0,0 +1,70 @@
+$(function() {
+    const $form = $('#signup-form');
+    const $message = $('#message');
+  
+    $form.submit(function(event) {
+      event.preventDefault();
+  
+      $message.html('');
+  
+      const data2 = $form.serialize();
+      console.log(data2);
+      
+      $.ajax({
+        url: 'http://localhost:3000/account/create',
+        type: 'POST',
+        data: data2,
+        withCredentials: true,
+
+        
+      }).then(() => {
+        $message.html('<span class="has-text-success">Success! You are now signed up in.</span>');
+      }).catch(() => {
+        $message.html('<span class="has-text-danger">Something went wrong and you were not signed up in. Check your email and password and your internet connection.</span>');
+      });
+    });
+  });
+ 
+
+    
+
+function onSignUp(googleUser) 
+  {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    //document.getElementById("demo").innerHTML ='Full Name: ' + profile.getName();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+$.ajax({
+  url: 'http://localhost:3000/account/create',
+  type: 'POST',
+  data:
+  {
+    "name":profile.getName(),
+    "pass":"1234"
+  } ,
+  withCredentials: true,
+});
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    var xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://http://localhost:3000/account/create');
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.onload = function() {
+console.log('Signed up as: ' + xhr.responseText);
+};
+// xhr.send('idtoken=' + id_token);
+}
+
+function signOut() 
+{
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
