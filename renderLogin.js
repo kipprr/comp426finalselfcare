af2bdb816bf6ab2a0bc680bcab812a59a3c0fb54
tsionc <36338302+tsionc@users.noreply.github.com>
2019-12-12 03:11:31 -0500

    homepage

--- renderLogin.js
+++ renderLogin.js
@@ -73,7 +73,7 @@ console.log('Signed in as: ' + xhr.responseText);
 //location.href=("homepage.html");
 };
 //location.href=("homepage.html");
-alert(`Welcome, ${profile1.getName()}`);
+// alert(`Welcome, ${profile1.getName()}`);
 location.href=("homepage.html");
 sessionStorage.setItem('user', `${profile1.getName()}`);
 };
